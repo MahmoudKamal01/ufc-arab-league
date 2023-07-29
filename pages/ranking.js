a09f812bf7d6a7b2eebcf51b81f373ee12571ac6
MahmoudKamal01 <dddd>
2023-07-29 23:35:30 +0300

    changed to render

--- pages/ranking.js
+++ pages/ranking.js
@@ -10,6 +10,7 @@ import Image from "next/image";
 const UserRankTable = () => {
   const [userRankData, setUserRankData] = useState([]);
   const [userPredictions, setUserPredictions] = useState([]);
+  const [userPredictionsName, setUserPredictionsName] = useState("User");
   const [eventData, setEventData] = useState([]);
   const [showPredictions, setShowPredictions] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
@@ -27,25 +28,11 @@ const UserRankTable = () => {
           "/api/v1/user/standings/64a8f41334a19ab5486bf513"
         );
         const standingsData = response.data.data;
-        console.log("aaa", standingsData);
+
         // Sort standings data based on score in descending order
         const sortedData = standingsData.sort((a, b) => a.rank - b.rank);
 
-        // // Assign ranks, handling ties
-        // let rank = 1;
-        // let prevScore = sortedData[0].score;
-        // sortedData.forEach((user, index) => {
-        //   if (user.score === prevScore) {
-        //     user.rank = rank;
-        //   } else {
-        //     user.rank = index + 1;
-        //     rank = index + 1;
-        //   }
-        //   prevScore = user.score;
-        // });
-
         setUserRankData(sortedData);
-        highlightedRowRef.current = userData ? userData.rank - 1 : null;
       } catch (error) {
         console.error("Failed to fetch standings data:", error);
       }
@@ -69,7 +56,8 @@ const UserRankTable = () => {
     }
   }, [username]);
 
-  const handleShowPredictions = async (userId) => {
+  const handleShowPredictions = async (userId, userName) => {
+    setUserPredictionsName(userName);
     if (isLoggedIn) {
       try {
         const userPredictionsResponse = await api.get(
@@ -121,12 +109,14 @@ const UserRankTable = () => {
               </tr>
             </thead>
             <tbody>
-              {userRankData.map((user) => (
+              {userRankData.map((user, index) => (
                 <tr
-                  ref={user.rank === userData?.rank ? highlightedRowRef : null}
+                  ref={
+                    user.userId?.name === username ? highlightedRowRef : null
+                  }
                   key={user._id}
                   className={
-                    user.rank === userData?.rank
+                    user.userId?.name === username
                       ? "bg-yellow-300"
                       : "hover:bg-gray-200"
                   }
@@ -136,7 +126,9 @@ const UserRankTable = () => {
                   <td className="py-2 text-center">{user.score}</td>
                   <td className="py-2 text-center">
                     <button
-                      onClick={() => handleShowPredictions(user._id)}
+                      onClick={() =>
+                        handleShowPredictions(user._id, user.userId.name)
+                      }
                       className="bg-gray-800 hover:bg-black text-white font-bold py-1 px-2 rounded text-sm lg:py-2 lg:px-4"
                     >
                       Show predictions
@@ -152,7 +144,8 @@ const UserRankTable = () => {
         <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-75 flex items-center justify-center">
           <div className="bg-white p-8 rounded">
             <h2 className="text-2xl font-bold mb-4">
-              {userData?.name} {"`"}s Predictions
+              {userPredictionsName}
+              {"'s"} Predictions
             </h2>
             <div className="overflow-x-auto overflow-y-auto max-h-96">
               <table className="min-w-full table-auto border border-gray-300 rounded">
